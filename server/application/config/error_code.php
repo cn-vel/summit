712dd76b076e18be386f6da534aaa30a0f2499bd
cn-vel <vel@codenatives.com>
2017-11-08 20:18:43 +0530

    Add files via upload

---
+++ server/application/config/error_code.php
@@ -0,0 +1,23 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/*
+| -------------------------------------------------------------------------
+| Custom Error Code
+| -------------------------------------------------------------------------
+|
+*/
+
+$config['error_code'] = array( 
+    600 => 'Unknown error',
+    601 => 'Requested json missing',
+    602 => 'Invalid register json',
+    603 => 'Invalid Organization code',
+    604 => 'No Attorney are select',
+    605 => 'Invalid Deposition',
+    606 => 'Image upload error',
+    607 => 'Invalid User',
+    608 => 'Transcript request Failed'
+);
+
+
+/* End of file routes.php */
+/* Location: ./application/config/error_code.php */
---
+++ server/application/controllers/Summitapi.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class SummitApi extends MY_Controller {
+
+	public function index()
+	{
+		
+	}
+}
